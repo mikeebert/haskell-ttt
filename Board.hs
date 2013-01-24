b78486124a7e7a388f98e05b9baac1d3a6afca53
Mike Ebert <mike@mikeebert.net>
2013-01-24 13:55:04 -0600

    Convert board to String array from Character array

--- Board.hs
+++ Board.hs
@@ -1,12 +1,11 @@
 module Board where
 import Data.Char
 
-emptyBoard :: [Char]
-emptyBoard = "123456789"
+emptyBoard = ["1","2","3","4","5","6","7","8","9"]
 
-availableSpaces board = filter isDigit board
+availableSpaces board = filter isNotPiece $ board
 
-placeMove :: (Char, Char) -> [Char] -> [Char]
+placeMove :: (String, String) -> [String] -> [String]
 placeMove (space, piece) board = map (swap (space,piece)) $ board
 
 {-swap :: (Char,Char) -> Char -> Char-}
@@ -14,15 +13,16 @@ swap spaceAndPiece boardSpace = if boardSpace == fst spaceAndPiece
                                   then snd spaceAndPiece
                                   else boardSpace
 
-full board = any isDigit board == False
+isNotPiece a = a /= "x" && a /= "o"
 
-winner board | all isPiece (firstRow board)  = head board
-             | all isPiece (secondRow board) = head (secondRow board)
-             | all isPiece (thirdRow board)  = last board
-             | otherwise                     = '0'
+full board = any isNotPiece board == False
 
-isPiece a = a == 'x' || a == 'o'
+{-winner board | all isPiece (firstRow board)  = head board-}
+             {-| all isPiece (secondRow board) = head (secondRow board)-}
+             {-| all isPiece (thirdRow board)  = last board-}
+             {-| otherwise                     = ""-}
 
-firstRow board  = [(board!!0), (board!!1), (board!!2)]
-secondRow board = [(board!!3), (board!!4), (board!!5)]
-thirdRow board  = [(board!!6), (board!!7), (board!!8)]
+
+firstRow board  = take 3 board
+{-secondRow board = [(board!!3), (board!!4), (board!!5)]-}
+{-thirdRow board  = [(board!!6), (board!!7), (board!!8)]-}
--- BoardSpec.hs
+++ BoardSpec.hs
@@ -2,39 +2,48 @@ import Test.Hspec
 import Test.QuickCheck
 import Board
 
-blankBoard = "123456789"
-fullBoard = ['x','o','x','x','o','x','o','x','o']
+blankBoard = ["1","2","3","4","5","6","7","8","9"]
+fullBoard = ["x","o","x","x","o","x","o","x","o"]
 
 main :: IO ()
 main = hspec $ do
   describe "Board" $ do
     it "returns an empty board" $ do
-      emptyBoard `shouldBe` "123456789"
+      emptyBoard `shouldBe` ["1","2","3","4","5","6","7","8","9"]
 
     it "places a move on an empty board" $ do
-      placeMove ('1','x') emptyBoard `shouldBe` "x23456789"
+      placeMove ("1","x") emptyBoard 
+      `shouldBe` ["x","2","3","4","5","6","7","8","9"]
 
     it "places a move on a board with other moves" $ do
-      placeMove ('4','x') "oxo4xoxx9" `shouldBe` "oxoxxoxx9"
+      placeMove ("4","x") ["o","x","o","4","o","x","x","8","9"] 
+        `shouldBe` ["o","x","o","x","o","x","x","8","9"]
 
     it "doesn't place a move in an occupied space" $ do
-      placeMove ('2','x') "1o3456789" `shouldBe` "1o3456789"
+      placeMove ("2","x") fullBoard `shouldBe` fullBoard
 
     it "returns a list of available spaces on empty board" $ do
-      availableSpaces emptyBoard `shouldBe` "123456789"
+      availableSpaces emptyBoard `shouldBe` ["1","2","3","4","5","6","7","8","9"]
 
-    it "returns True for a full board" $ do
+    it "returns an empty list of available spaces on a full board" $ do
+      availableSpaces fullBoard `shouldBe` []
+
+    it "#full returns True for a full board" $ do
       full fullBoard `shouldBe` True
 
-    it "returns winner for horizontal win in first row" $ do
-      winner "xxx456789" `shouldBe` 'x'
+    it "#full returns False for an empty board" $ do
+      full emptyBoard `shouldBe` False
 
-    it "returns winner for horizontal win in second row" $ do
-      winner "123xxx789" `shouldBe` 'x'
+    {-it "returns winner for horizontal win in first row" $ do-}
+      {-winner ["x","x","x","4","5","6","7","8","9"] -}
+        {-`shouldBe` "x"-}
 
-    it "returns winner for horizontal win in final row" $ do
-      winner "123456xxx" `shouldBe` 'x'
+    {-it "returns winner for horizontal win in second row" $ do-}
+      {-winner "123xxx789" `shouldBe` "x"-}
 
-    it "returns nil for no winner" $ do
-      winner blankBoard `shouldBe` '0'
+    {-it "returns winner for horizontal win in final row" $ do-}
+      {-winner "123456xxx" `shouldBe` "x"-}
+
+    {-it "returns nil for no winner" $ do-}
+      {-winner blankBoard `shouldBe` "0"-}
     
