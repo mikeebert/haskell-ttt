fcdff4fd8e673128f5a57a0df722a3e9459afe43
Mike Ebert <mike@mikeebert.net>
2013-01-26 14:32:56 -0600

    First run at Minimax. Slow and not quite the right values.

--- src/Ai.hs
+++ src/Ai.hs
@@ -4,44 +4,36 @@ import Player
 import Board
 import Data.Char
 
-getDumbMove list = head list
-
-{-minimaxScore board player opponent depth = let score = value board player opponent depth-}
-
-
-value :: [String] -> Player -> Player -> Integer -> Integer
-value board player opponent depth | winner board == piece player      = 100 - depth
-                                  | winner board == piece opponent    = -100 + depth
-                                  | tieGame board                        = 0
-                                  | otherwise                         = -1
-
-
+firstAvailableMove list = head list
 
 {-getAiMove :: [String] -> Player -> Player -> String-}
-{-getAiMove board player opponent = let scores = map (minimaxScore player opponent 1) -}
-                                                 {-(map (makeMove newBoard (piece player)) (availableSpaces newBoard))-}
-                                      {-moves = availableSpaces board-}
-                                      {-valuesOfMoves = zip scores moves-}
-                                  {-in bestMove valuesOfMoves-}
-
-{-RETURNS SCORES: -}
-{-map (minimaxScore player1 player1 1) (map (makeMove newBoard (piece player1)) (availableSpaces newBoard))-}
+getAiMove board player opponent = let scores = map (minimaxScore player opponent 1) (map (makeMove board (piece player)) (availableSpaces board))
+                                      moves = availableSpaces board
+                                      valuesOfMoves = zip scores moves
+                                  in bestMove valuesOfMoves
 
 minimaxScore :: Player -> Player -> Integer -> [String] -> Integer
 minimaxScore player opponent depth board = let score = value board player opponent depth
-                                           in if score /= -10
+                                           in if score /= -1
                                                 then score
-                                                else score
+                                                else bestScore opponent (map (minimaxScore opponent player (succ depth)) 
+                                                       (map (makeMove board (piece player)) (availableSpaces board)))
 
-                                           {-if score /= -1 -}
-                                             {-then score -}
-                                             {-else minimaxScore -}
-
+bestScore player scores = if strategy player == "max"
+                            then maximum scores
+                            else minimum scores
 
 bestMove :: [(Integer, String)] -> String
 bestMove scoresMoves = let bestScore = maximum (fst (unzip scoresMoves))
                        in moveFor bestScore scoresMoves
 
+value :: [String] -> Player -> Player -> Integer -> Integer
+value board player opponent depth | winner board == piece player   = 100 - depth
+                                  | winner board == piece opponent = -100 + depth
+                                  | tieGame board                  = 0
+                                  | otherwise                      = -1
+
+moveFor :: Integer -> [(Integer, String)] -> String
 moveFor score (x:xs) = if score == (fst x)
                          then snd x
                          else moveFor score xs
--- test/AiTest.hs
+++ test/AiTest.hs
@@ -3,6 +3,7 @@ module AiTest where
 import Test.Hspec
 import Test.QuickCheck
 import Player
+import Board
 import Ai
 
 main :: IO ()
@@ -21,8 +22,8 @@ maxXplayer = Player {piece = "x", strategy = "max", startingScore = -5}
 spec :: Spec
 spec = do
   describe "Ai" $ do
-    it "returns a dumb move" $ do
-      listIncludes (getDumbMove ["1","2","3"]) ["1","2","3"] `shouldBe` True
+    it "returns first available move" $ do
+      firstAvailableMove ["1","2","3"] `shouldBe` "1"
 
     it "returns the value of a won board in 1 move" $ do
       value wonXBoard maxXplayer minOplayer 1 `shouldBe` 99
@@ -36,11 +37,31 @@ spec = do
     it "returns -1 as the value for a game in progress" $ do
       value ["1","2","3","4","5","7","8","9"] maxXplayer minOplayer 0 `shouldBe` -1
 
+    it "returns move with highest score" $ do
+      bestMove [(95,"1"), (-99,"2"), (-1,"3"), (96,"4"), (-1,"5")] `shouldBe` "4" 
+
+    it "returns best score for max" $ do
+      bestScore maxXplayer [0,1,99,-5] `shouldBe` 99
+
+    it "returns the best score min" $ do
+      bestScore minOplayer [1,0,-1,-5] `shouldBe` -5
+
     it "scores a won board for the max player" $ do
       minimaxScore maxXplayer minOplayer 5 wonXBoard `shouldBe` 95
 
-    it "returns move with highest score" $ do
-      bestMove [(95,"1"), (-99,"2"), (-1,"3"), (96,"4"), (-1,"5")] `shouldBe` "4" 
+    it "scores a won board for the min player" $ do
+      minimaxScore maxXplayer minOplayer 2 wonOBoard `shouldBe` -98
+
+    it "scores a tie game as 0" $ do
+      minimaxScore maxXplayer minOplayer 8 drawGameBoard `shouldBe` 0
+
+    it "chooses a win in one move" $ do
+      getAiMove ["x","x","3","4","5","6","7","8","9"] maxXplayer minOplayer 
+        `shouldBe` "3"
+
+    it "chooses the center for second move if opponent chooses corner" $ do
+      getAiMove ["o","2","3","4","5","6","7","8","9"] maxXplayer minOplayer 
+        `shouldBe` "5"
 
 listIncludes n list = length (filter (== n) list) >= 1
 
