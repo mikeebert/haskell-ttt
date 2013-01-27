ec9692c0f41b6624ba5409c50822ec070df80e9d
Mike Ebert <mike@mikeebert.net>
2013-01-27 17:55:21 -0600

    Updates Ai to handle setUp of Max and Min Players

--- src/Ai.hs
+++ src/Ai.hs
@@ -5,26 +5,33 @@ import Board
 import Data.Char
 
 inProgressScore = -1
+tieGameScore    = 0
+depthLimit      = 5
+depthLimitScore = -5
+maxStrategy = "max"
 
 firstAvailableMove list = head list
 
 getAiMove :: [String] -> Player -> Player -> String
-getAiMove board player opponent = let scores = map (minimaxScore player opponent 1) (potentialMoves player board)
+getAiMove board player opponent = let maxPlayer = setupMax player
+                                      minPlayer = setupMin opponent
+                                      scores = map (minimaxScore maxPlayer minPlayer 1) 
+                                                   (potentialMoves maxPlayer board)
                                       moves = availableSpaces board
                                       valuesOfMoves = zip scores moves
                                   in bestMove valuesOfMoves
 
-minimaxScore :: Player -> Player -> Integer -> [String] -> Integer
+minimaxScore :: MinimaxPlayer -> MinimaxPlayer -> Integer -> [String] -> Integer
 minimaxScore player opponent depth board = let score = value board player opponent depth
                                            in if score /= inProgressScore
                                                 then score
                                                 else bestScore opponent (map (minimaxScore opponent player (succ depth)) 
-                                                                          (potentialMoves opponent board))
+                                                                             (potentialMoves opponent board))
 
-potentialMoves :: Player -> [String] -> [[String]]
-potentialMoves player board = map (makeMove board (piece player)) (availableSpaces board)
+potentialMoves :: MinimaxPlayer -> [String] -> [[String]]
+potentialMoves player board = map (makeMove board (symbol player)) (availableSpaces board)
 
-bestScore player scores = if strategy player == "max"
+bestScore player scores = if strategy player == maxStrategy
                             then maximum scores
                             else minimum scores
 
@@ -32,13 +39,14 @@ bestMove :: [(Integer, String)] -> String
 bestMove scoresMoves = let bestScore = maximum (fst (unzip scoresMoves))
                        in moveFor bestScore scoresMoves
 
-value :: [String] -> Player -> Player -> Integer -> Integer
-value board player opponent depth | winner board == piece player   = score player depth
-                                  | winner board == piece opponent = score opponent depth
-                                  | tieGame board                  = 0
-                                  | otherwise                      = inProgressScore 
+value :: [String] -> MinimaxPlayer -> MinimaxPlayer -> Integer -> Integer
+value board player opponent depth | winner board == symbol player   = score player depth
+                                  | winner board == symbol opponent = score opponent depth
+                                  | tieGame board                   = tieGameScore 
+                                  | depth > depthLimit              = depthLimitScore
+                                  | otherwise                       = inProgressScore 
 
-score player depth = if strategy player == "max"
+score player depth = if strategy player == maxStrategy
                        then 100 - depth
                        else -100 + depth
 
@@ -48,4 +56,16 @@ moveFor score (x:xs) = if score == (fst x)
                          else moveFor score xs
 
 makeMove :: [String] -> String -> String -> [String]
-makeMove board piece space = placeMove (space, piece) board
+makeMove board symbol space = placeMove (space, symbol) board
+
+data MinimaxPlayer = MinimaxPlayer {strategy :: String,
+                                    symbol :: String
+                                   } deriving (Show)
+
+setupMax player = MinimaxPlayer {strategy = "max", 
+                                 symbol = (piece player)}
+
+setupMin player = MinimaxPlayer {strategy = "min",
+                                 symbol = (piece player)}
+
+
--- test/AiTest.hs
+++ test/AiTest.hs
@@ -16,8 +16,11 @@ drawGameBoard = ["x","o","x",
                  "x","o","x",
                  "o","x","o"]
 
-maxXplayer = Player {piece = "x", strategy = "max"}
-minOplayer = Player {piece = "o", strategy = "min"}
+maxXplayer = MinimaxPlayer {symbol = "x", strategy = "max"}
+minOplayer = MinimaxPlayer {symbol = "o", strategy = "min"}
+
+xPlayer = Player {piece = "x", kind = "computer"}
+oPlayer = Player {piece = "o", kind = "human"}
 
 spec :: Spec
 spec = do
@@ -59,23 +62,21 @@ spec = do
       minimaxScore maxXplayer minOplayer 1 ["o","o","3","x","x","o","x","o","x"] `shouldBe` -98
 
     it "chooses a win in one move" $ do
-      getAiMove ["x","x","3","4","5","6","7","8","9"] maxXplayer minOplayer 
+      getAiMove ["x","x","3","4","5","6","7","8","9"] xPlayer oPlayer 
         `shouldBe` "3"
 
-    {-it "ranks potential wins, losses and draws" $ do-}
-      {-map (minimaxScore maxXplayer minOplayer 1)  -}
-          {-(map (makeMove ["x","x","3","o","5","o","o","o","9"] (piece maxXplayer))-}
-               {-(availableSpaces ["x","x","3","o","5","x","o","o","9"]))-}
-      {-`shouldBe` [99,-98,-98]-}
-
     it "chooses the center for second move if opponent chooses corner" $ do
-      getAiMove ["o","2","3","4","5","6","7","8","9"] maxXplayer minOplayer 
+      getAiMove ["o","2","3","4","5","6","7","8","9"] xPlayer oPlayer 
         `shouldBe` "5"
+
+    it "chooses a corner for the opening move" $ do
+      moveIsInList (getAiMove emptyBoard xPlayer oPlayer) ["1","3","7","9"]
+        `shouldBe` True
 
     it "blocks L setup" $ do
       moveIsInList (getAiMove ["o","2","3",
                                "4","x","6",
-                               "7","o","9"] maxXplayer minOplayer)
+                               "7","o","9"] xPlayer oPlayer)
         ["4","7","6","9"] `shouldBe` True
 
 moveIsInList n list = length (filter (== n) list) >= 1
