1875252caf5d7b07235b9443833b40e9058a7ae0
Mike Ebert <mike@mikeebert.net>
2013-01-27 17:57:57 -0600

    Adds Tie and hasWinner Booleans

--- src/Board.hs
+++ src/Board.hs
@@ -1,4 +1,6 @@
 module Board where
+
+import Prelude
 import Data.List
 import Data.Char
 
@@ -14,29 +16,31 @@ swap spaceAndPiece boardSpace = if boardSpace == fst spaceAndPiece
                                   then snd spaceAndPiece
                                   else boardSpace
 
-full board = null (availableSpaces board)
-
 winner board | haveWinner (rows board)      = winningSymbol (rows board)
              | haveWinner (columns board)   = winningSymbol (columns board)
              | haveWinner (diagonals board) = winningSymbol (diagonals board)
              | otherwise                    = ""
 
-haveWinner line = any allEqual line
-
-{-!!!!extract into Game-}
-tieGame board = null (winner board) && full board
+haveWinner lines = any allEqual lines
 
 winningSymbol :: [[String]] -> String
 winningSymbol rowsOrColumns = head (head (filter allEqual rowsOrColumns))
 
+hasWinner board = null (winner board) == False
+tieGame board = null (winner board) && full board
+full board = null (availableSpaces board)
+
 allEqual list = length list == (length $ takeWhile (== head list) list)
 
-rows board = [(take 3 board),(take 3 (drop 3 board)),(drop 6 board)]
+rows board = [(take 3 board),
+              (take 3 (drop 3 board)),
+              (drop 6 board)]
 
 columns board = [[(board!!0),(board!!3),(board!!6)],
                  [(board!!1),(board!!4),(board!!7)],
                  [(board!!2),(board!!5),(board!!8)]]
 
-diagonals board = [[board!!0,board!!4,board!!8],[board!!2,board!!4,board!!6]]
+diagonals board = [[board!!0,board!!4,board!!8],
+                   [board!!2,board!!4,board!!6]]
 
 isAvailable a = isDigit (head a)
