268f7baa47ec0adb3b43db644a6c98cab1463112
Mike Ebert <mike@mikeebert.net>
2013-01-27 20:55:08 -0600

    Factors out CheckGameStatus from Ai and Human Move scenarios

--- src/ConsoleRunner.hs
+++ src/ConsoleRunner.hs
@@ -7,8 +7,8 @@ import Ai
 import Data.List
 
 computer = "computer"
-computerSelection = "1"
 human = "human"
+computerSelection = "1"
 humanSelection = "2"
 playerOptions = (computerSelection, humanSelection)
 x = "x"
@@ -20,8 +20,6 @@ start = do
   player2type <- uiGetPlayer o playerOptions
   let player1 = setupPlayer x (selectionFor player1type) 
       player2 = setupPlayer o (selectionFor player2type)
-  putStrLn ("Player 1 is a " ++ (kind player1))
-  putStrLn ("Player 2 is a " ++ (kind player2))
   gameLoop player1 player2 emptyBoard
 
 gameLoop player nextPlayer board = do
@@ -30,6 +28,9 @@ gameLoop player nextPlayer board = do
     then humanMoveScenario player nextPlayer board
     else aiMoveScenario player nextPlayer board
 
+checkGameStatus player nextPlayer board = if gameOver board
+                                            then endOfGame board
+                                            else gameLoop nextPlayer player board
 {-THIS ALSO WORKS-}
 {-gameLoop player nextPlayer board | (kind player) == computer = do uiDisplay (formatted board)-}
                                                                   {-aiMoveScenario player nextPlayer board-}
@@ -38,16 +39,13 @@ gameLoop player nextPlayer board = do
 
 aiMoveScenario player nextPlayer board = let move = getAiMove board player nextPlayer
                                              updatedBoard = placeMove (move, (piece player)) board
-                                         in if gameOver updatedBoard
-                                              then endOfGame updatedBoard
-                                              else gameLoop nextPlayer player updatedBoard
+                                         in checkGameStatus player nextPlayer updatedBoard
 
 humanMoveScenario player nextPlayer board = do
   move <- uiGetMove (availableSpaces board)
   let updatedBoard = placeMove (move, (piece player)) board
-  if gameOver updatedBoard
-     then endOfGame updatedBoard
-     else gameLoop nextPlayer player updatedBoard
+  checkGameStatus player nextPlayer updatedBoard
+
 
 {-THIS AND THE METHODS BELOW DO NOT WORK BECAUSE OF IO IMPURITY-}
 {-gameLoop player nextPlayer board = do-}
